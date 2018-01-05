f8775c8608d77c0deefda2cf782248ab3d04bcbc
Alice M. Yoon <misun6312@gmail.com>
2018-01-05 08:21:06 -0500

    optimization updated

--- src/model_optimization.jl
+++ src/model_optimization.jl
@@ -1,52 +1,122 @@
-function InitParams(seed_mode=1)
+# updating for generalizing the model. 
+# user will give args to init parameters 
+# use 'dictionary' -> each parameter has different range
+
+# utilize make_dict and defaults.
+
+function InitParams(args, seed_mode=1)
 
     # Parameters (match the parameter order with original code)
+    # --> order doesn't matter now, it will be matched with the order of parameters
+
+    x_val = zeros(length(args))
 
     # random seed    
     if seed_mode == 1
-        lam = randn()*.01; 
-        sigma_a = rand()*50.; sigma_s = rand()*50.; sigma_i = rand()*30.; 
-        B = rand()*12.+5.; 
-        phi = rand(); tau_phi = 0.695*rand()+0.005; 
-        bias = randn(); lapse = rand()*.5;
-    
-        params = [lam, sigma_a, sigma_s, sigma_i, B, phi, tau_phi, bias, lapse] 
+        rseed_param = Dict("sigma_a" => rand()*50., 
+        "sigma_s_R" => rand()*50.,
+        "sigma_s_L" => rand()*50.,
+        "sigma_i" => rand()*30.,
+        "lambda" => randn()*.01,
+        "B" => rand()*12.+5.,
+        "bias" => randn(),
+        "phi" => rand(),
+        "tau_phi" => 0.695*rand()+0.005,
+        "lapse_R" => rand()*.5,
+        "lapse_L" => rand()*.5,
+        "input_gain_weight" => rand())
 
+        for i in 1:length(args)
+            x_val[i] = rseed_param[args[i]]
+        end
     # bing's rat avg parameter set
     elseif seed_mode == 2
         params = [-0.2767, 2.0767, 75.9600, 1.9916, 8.9474, 0.1694, 0.0964, -0.0269, 0.0613]
+        defaults_param = Dict("sigma_a" => 2.0767, 
+        "sigma_s_R" => 75.9600,
+        "sigma_s_L" => 75.9600,
+        "sigma_i" => 1.9916,
+        "lambda" => -0.2767,
+        "B" => 8.9474,
+        "bias" => -0.0269,
+        "phi" => 0.1694,
+        "tau_phi" => 0.0964,
+        "lapse_R" => 0.0613,
+        "lapse_L" => 0.0613,
+        "input_gain_weight" => 0.5)
+
+        for i in 1:length(args)
+            x_val[i] = defaults_param[args[i]]
+        end
 
     # simple fixed parameter set    
     elseif seed_mode == 3
-        lam = -0.0005; sigma_a = 1.; sigma_s = 0.1; sigma_i = 0.2; 
-        B = 6.1; phi = 0.3; tau_phi = 0.1; bias = 0.1; lapse = 0.05*2;
+        simple_param = Dict("sigma_a" => 1., 
+        "sigma_s_R" => 0.1,
+        "sigma_s_L" => 0.1,
+        "sigma_i" => 0.2,
+        "lambda" => -0.0005,
+        "B" => 6.1,
+        "bias" => 0.1,
+        "phi" => 0.3,
+        "tau_phi" => 0.1,
+        "lapse_R" => 0.1,
+        "lapse_L" => 0.1,
+        "input_gain_weight" => 0.5)
 
-        params = [lam, sigma_a, sigma_s, sigma_i, B, phi, tau_phi, bias, lapse] 
+        for i in 1:length(args)
+            x_val[i] = simple_param[args[i]]
+        end
     end
 
-    return params   
+    return x_val   
 end
 
-function ModelFitting(params, ratdata, ntrials)
-    # Parameter Constraints 
-    l = [-5., 0.,   0.,   0.,  2.,  0.01, 0.005, -5., 0.]
-    u = [+5., 200., 200., 40., 32.,  2.5,    1.,  5., 1.]
+function ModelFitting(args, x_init, ratdata, ntrials)
+    l_b = Dict("sigma_a" => 0., 
+        "sigma_s_R" => 0.,
+        "sigma_s_L" => 0.,
+        "sigma_i" => 0.,
+        "lambda" => -5.,
+        "B" => 5.,
+        "bias" => -5.,
+        "phi" => 0.01,
+        "tau_phi" => 0.005,
+        "lapse_R" => 0.,
+        "lapse_L" => 0.,
+        "input_gain_weight" => 0.)
 
-    function LL_f(params::Vector)
+    u_b = Dict("sigma_a" => 200., 
+        "sigma_s_R" => 200.,
+        "sigma_s_L" => 200.,
+        "sigma_i" => 40.,
+        "lambda" => 5.,
+        "B" => 25.,
+        "bias" => 5.,
+        "phi" => 1.2,
+        "tau_phi" => 1.5,
+        "lapse_R" => 1.,
+        "lapse_L" => 1.,
+        "input_gain_weight" => 1.)
+
+    function LL_f(x_init::Vector)
         LLs = SharedArray(Float64, ntrials)
-        return ComputeLL(LLs, params, ratdata["rawdata"], ntrials)
+        return ComputeLL(LLs, ratdata["rawdata"], ntrials, args, x_init)
     end
 
     # updated for julia v0.6 (in-place order)
-    function LL_g!{T}(grads::Vector{T}, params::Vector{T})
-        LL, LLgrad = ComputeGrad(params, ratdata["rawdata"], ntrials)
-        for i=1:length(params)
+    function LL_g!{T}(grads::Vector{T}, x_init::Vector{T})
+        LL, LLgrad = ComputeGrad(ratdata["rawdata"], ntrials, args, x_init)
+
+        for i=1:length(x_init)
             grads[i] = LLgrad[i]
         end
     end
 
-    function LL_fg!(params::Vector, grads)
-        LL, LLgrad = ComputeGrad(params, ratdata["rawdata"], ntrials)
+    function LL_fg!(x_init::Vector, grads)
+        LL, LLgrad = ComputeGrad(ratdata["rawdata"], ntrials, args, x_init)
+
+## so far..
         for i=1:length(params)
             grads[i] = LLgrad[i]
         end
